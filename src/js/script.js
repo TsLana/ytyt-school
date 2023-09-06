3a75d182fa278c263b724f51fecdbae6d858f286
Ruslana Tsokota <ruslana321132@gmail.com>
2023-09-06 23:06:51 +0300

    ytyt-1 add header and first section

--- src/js/script.js
+++ src/js/script.js
@@ -1,5 +1,15 @@
-$(document).ready(function(){
-    $('.hamburger').click(function(){
-        $('.hamburger').toggleClass('menu__close');
+const hamburger = document.querySelector('.hamburger'),
+      submenu = document.querySelector('.submenu'),
+      closeElem = document.querySelector('.submenu__close');
+
+      hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        submenu.classList.toggle('submenu_active');
     });
-});
+
+    closeElem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            submenu.classList.toggle('submenu_active');
+        })
+})
